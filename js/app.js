03975e182c49000f12770115d079d2d4d752e199
kfarfan <kevinfarfan@gmail.com>
2018-09-15 14:07:56 -0400

    adding npc and gravity

--- js/app.js
+++ js/app.js
@@ -11,7 +11,9 @@ let right = document.querySelector('#right');
 const header = document.querySelector(`.game__header`);
 let index = 0;
 let interval = 0;
+const track = document.querySelector('#road');
 const trees = ['1set.png', '2set.png', '3set.png', '4set.png', '5set.png'];
+const tracks = ['track1.png', 'track2.png', 'track3.png', 'track4.png', 'track5.png'];
 const player = document.querySelector('.player');
 player.style.backgroundImage = `url(../images/player1.png)`;
 let moves = 200;
@@ -26,6 +28,7 @@ let imageChange = function () {
         interval++;
     }
     left.style.backgroundImage = `url(../images/${trees[index]})`;
+    track.style.backgroundImage = `url(../images/${tracks[index]})`;
     right.style.backgroundImage = `url(../images/${trees[index]})`;
     
     if(gameover) {
@@ -35,17 +38,18 @@ let imageChange = function () {
     }
 }
 
+
 let tree = setInterval(imageChange, 150);
 
 
 
 let playerMoves = function playerMove (e) {
     if(e.keyCode == '37' && moves !== 0){
-        moves-=50;
+        moves-=100;
         player.style.marginLeft = ''+moves+'px';
         }
     if(e.keyCode == '39' && moves !== 400){
-         moves+=50;
+         moves+=100;
          player.style.marginLeft = ''+moves+'px';
          }
 }
