4928e25d2412f93d13d22c4bbce26f24ed1c6e11
kfarfan <kevinfarfan@gmail.com>
2018-09-14 21:38:50 -0400

    adding player controls

--- js/app.js
+++ js/app.js
@@ -12,7 +12,9 @@ const header = document.querySelector(`.game__header`);
 let index = 0;
 let interval = 0;
 const trees = ['1set.png', '2set.png', '3set.png', '4set.png', '5set.png'];
-
+const player = document.querySelector('.player');
+player.style.backgroundImage = `url(../images/player1.png)`;
+let moves = 200;
 const gameover = false;
 
 
@@ -23,8 +25,8 @@ let imageChange = function () {
         index = 0;
         interval++;
     }
-    left.style.backgroundImage = 'url(../images/'+trees[index]+')';
-    right.style.backgroundImage = 'url(../images/'+trees[index]+')';
+    left.style.backgroundImage = `url(../images/${trees[index]})`;
+    right.style.backgroundImage = `url(../images/${trees[index]})`;
     
     if(gameover) {
         clearInterval(tree);
@@ -36,9 +38,21 @@ let imageChange = function () {
 let tree = setInterval(imageChange, 150);
 
 
-let theme = new Audio('../sounds/linkintheme.mp3');
 
-theme.play();
+let playerMoves = function playerMove (e) {
+    if(e.keyCode == '37' && moves !== 0){
+        moves-=20;
+        player.style.marginLeft = ''+moves+'px';
+        }
+    if(e.keyCode == '39' && moves !== 400){
+         moves+=20;
+         player.style.marginLeft = ''+moves+'px';
+         }
+}
+
+document.addEventListener('keydown', playerMoves);
 
 
-const player = url('.//images/player1.png');
+// let theme = new Audio('../sounds/linkintheme.mp3');
+
+// theme.play();
